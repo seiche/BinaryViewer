6c3bbcc2e9f9bf4a8a2add2b7c047503aafd33f8
seiche <seichestream@gmail.com>
2015-09-15 08:46:33 +0900

    uint array

--- js/main.js
+++ js/main.js
@@ -10,7 +10,28 @@ function main(){
 function file_select(){
   var chooser = document.getElementById("fileDialog");
   chooser.addEventListener("change", function(evt) {
-    console.log(this.value);
+    var file = evt.target.files[0];
+    var reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    var div = document.getElementById("viewer");
+    reader.onload = function(){
+      var buffer = this.result;
+      var arr = new Uint8Array(buffer);
+      var byte, ul, li;
+      var space = 0;
+      div.innerHTML = "";
+      for(var i = 0; i < arr.length; i++){
+        byte = arr[i].toString(16);
+        if(byte.length < 2) byte = "0" + byte;
+        if(space == 0){
+          ul = document.createElement("ul");
+          ul.setAttribute("class", "line");
+        }
+        if(space % 4 == 0){
+          ul.appendChild(li);
+        }
+      }
+    }
   });
 
   chooser.click();
