2c799864ff990c2781ba31fbbed17f9bedc69361
seiche <seichestream@gmail.com>
2015-09-16 23:41:39 +0900

    more updates

--- js/main.js
+++ js/main.js
@@ -14,22 +14,60 @@ function file_select(){
     var reader = new FileReader();
     reader.readAsArrayBuffer(file);
     var div = document.getElementById("viewer");
+    var offdiv = document.getElementById("offset");
+    div.innerHTML = "";
+    offdiv.innerHTML = "";
     reader.onload = function(){
       var buffer = this.result;
       var arr = new Uint8Array(buffer);
       var byte, ul, li;
       var space = 0;
-      div.innerHTML = "";
+      var line = 0;
+      var array = [];
+      var file = [];
+      var str = "";
+
       for(var i = 0; i < arr.length; i++){
         byte = arr[i].toString(16);
         if(byte.length < 2) byte = "0" + byte;
-        if(space == 0){
-          ul = document.createElement("ul");
-          ul.setAttribute("class", "line");
+        if(space == 4){
+          space = 0;
+          line++;
+          array.push(str);
+          str = "";
         }
-        if(space % 4 == 0){
-          ul.appendChild(li);
+
+        if(line == 4){
+          line = 0;
+          file.push(array);
+          array = [];
         }
+
+        str += byte;
+        space++;
+      }
+
+      for(var i = 0; i < file.length; i++){
+        var ul = document.createElement("ul");
+        ul.setAttribute("class", "line");
+        var tb = document.createElement("table");
+        ul.appendChild(tb);
+        var row = tb.insertRow(-1);
+        var offnum = (i * 16).toString(16);
+        while(offnum.length < 6){
+          offnum = "0" + offnum;
+        }
+        offnum = "0x" + offnum;
+        var line = document.createElement("li");
+        line.textContent = offnum;
+        offdiv.appendChild(line);
+
+        for(var k = 0; k < file[i].length; k++){
+          var cell = row.insertCell(-1);
+          cell.textContent = file[i][k];
+        }
+
+        div.appendChild(ul);
       }
     }
   });
